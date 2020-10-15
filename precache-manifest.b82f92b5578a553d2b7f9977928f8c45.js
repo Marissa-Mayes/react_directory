27e7e3bbcc09294e64bfe4bdfbb3e624589f6883
marissa-mayes <marissa.mayes1289@gmail.com>
2020-10-14 23:26:46 -0500

    Updates

---
+++ precache-manifest.b82f92b5578a553d2b7f9977928f8c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53423bd8f15e6c29306af6439f82479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da799b7be7249cfb78d",
+    "url": "/static/css/main.862376e3.chunk.css"
+  },
+  {
+    "revision": "fc7af8ac223172f471c5",
+    "url": "/static/js/2.d80309d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d80309d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da799b7be7249cfb78d",
+    "url": "/static/js/main.5aeff25d.chunk.js"
+  },
+  {
+    "revision": "1a9355e74d03dc4c7068",
+    "url": "/static/js/runtime-main.e0225f3c.js"
+  }
+]);
